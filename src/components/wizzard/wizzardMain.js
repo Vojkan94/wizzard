7547b3f3b4cffd8f56d1f8c8825c2ae1327b5983
Vojkan <t.vojkan@gmail.com>
2022-06-11 15:33:10 +0200

    in progress

--- src/components/wizzard/wizzardMain.js
+++ src/components/wizzard/wizzardMain.js
@@ -23,7 +23,7 @@ function WizzardMain() {
           <div className="step-name">Forth</div>
         </div>
       </div>
-      <div className="genre-list-wrapper">
+      {/* <div className="genre-list-wrapper">
         <div className="genre-button">Genre 1</div>
         <div className="genre-button">Genre 1</div>
         <div className="genre-button">Genre 1</div>
@@ -34,6 +34,85 @@ function WizzardMain() {
         <div className="genre-button">Genre 1</div>
         <div className="genre-button">Genre 1</div>
         <div className="genre-button">Genre 1</div>
+      </div> */}
+      {/* <div className="new-subgenre-wrapper">
+        <input className="input-field" placeholder="Subgenre name" />
+        <label style={{ cursor: 'pointer' }}>
+          <input type="checkbox" /> Description is required for this subgenre
+        </label>
+      </div> */}
+
+      <div className="book-info-wrapper">
+        <label className="info-label">
+          Book title
+          <input className="input-field" placeholder="Book title" />
+        </label>
+        <label className="info-label">
+          Author
+          <select defaultValue="-">
+            <option disabled value="-">
+              Author
+            </option>
+            <option>Ivo</option>
+          </select>
+        </label>
+        <label className="info-label">
+          ISBN
+          <input className="input-field" placeholder="ISBN" />
+        </label>
+        <label className="info-label">
+          Publisher
+          <select defaultValue="-">
+            <option disabled value="-">
+              Publisher
+            </option>
+            <option>Vulkan</option>
+          </select>
+        </label>
+        <label className="info-label">
+          Date published
+          <input
+            style={{ textTransform: 'uppercase' }}
+            className="input-field"
+            type="date"
+          />
+        </label>
+        <label className="info-label">
+          Number of pages
+          <input
+            type="number"
+            className="input-field"
+            placeholder="Number of pages"
+          />
+        </label>
+        <label className="info-label">
+          Format
+          <select defaultValue="-">
+            <option disabled value="-">
+              Format
+            </option>
+            <option>A5</option>
+          </select>
+        </label>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <label className="info-label" style={{ width: '48%' }}>
+            Edition
+            <input className="input-field" placeholder="Edition" />
+          </label>
+          <label className="info-label" style={{ width: '48%' }}>
+            Edition language
+            <select defaultValue="-">
+              <option disabled value="-">
+                Edition language
+              </option>
+              <option>srpski</option>
+            </select>
+          </label>
+        </div>
+        <label className="info-label">
+          Description
+          <textarea rows="4" placeholder="Type the description..." />
+        </label>
       </div>
       <div className="buttons-wrapper">
         <button className="step-button back">&#60; Back</button>
